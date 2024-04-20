3e9df26abe63aeaf2d05eab62af6512cd17f7558
vinay <vinaykhanna768@gmail.com>
2024-04-20 18:04:44 +0530

    Second commit

---
+++ read_name_age.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#read name form user
+#read name age from user
+
+
+echo "Enter your name:"
+read name
+sleep 1
+echo "Enter your age:"
+read age
+sleep 1
+echo "Hi $name your age is $age great"
